1eb9ee6e17204ab40bad0ad43266728605ec46f1
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-26 15:43:04 +0200

    Aggiunge div tramite js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 `use strict`;
+//array immagini
+let imageList = [
+    '../img/01.jpg',
+    '../img/02.jpg',
+    '../img/03.jpg',
+    '../img/04.jpg',
+    '../img/05.jpg'
+];
+
+const boxImg = document.getElementById('box-img');
+
+for(let i=0;i<imageList.length;i++){
+    const imgPrint = imageList[i];
+    let divImg = document.createElement('div');
+    let img = document.createElement('img');
+    img.setAttribute("src", imageList[i]);
+    if(imageList[0] === imgPrint){
+
+        
+    }
+        boxImg.append(divImg);
+        boxImg.append(img);
+        console.log(divImg)
+        console.log(img)
+
+    
+}
