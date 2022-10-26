27a04cda4bc458118b3cd87b9a96a8f20f5e11ab
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-26 17:35:13 +0200

    Aggiunge modifica all'intero codice

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 `use strict`;
 //array immagini
-let imageList = [
+let imagesList = [
     '../img/01.jpg',
     '../img/02.jpg',
     '../img/03.jpg',
@@ -8,23 +8,24 @@ let imageList = [
     '../img/05.jpg'
 ];
 
-const boxImg = document.getElementById('box-img');
+let items=document.querySelector('.items');
+let active=0; 
+for (let i = 0; i < imagesList.length; i++){
+    items.innerHTML +=`<div class="item"><img src="${imagesList[i]}" alt="img1"></div>`;             
+}           
+document.querySelector('.item').classList.add('d-block');
 
-for(let i=0;i<imageList.length;i++){
+document.querySelector('.after').addEventListener("click" , function(){
+    let img = document.querySelector('.item');
 
-    let imgPrint = imageList[i];
-    let divImg = document.createElement('div');
-    let img = document.createElement('img');
-    img.setAttribute("src", imageList[i]);
-
-    if(imageList[0] === imgPrint){
-        divImg.classList.add('d-block')   
+    if(active == img.length-1){
+        img[active].classList.remove('d-block');
+        active=0;
+        img[active].classList.add('d-block');
     }
-    
-    boxImg.append(divImg);
-    divImg.append(img);
-    console.log(divImg)
-    console.log(img)
-
-    
-}
+    else{
+        img[active].classList.remove('d-block');
+        active= active+1;
+        img[active].classList.add('d-block');
+        }
+    });
